8984f7025dac8076bd25bcd1bc9b95be262fe71f
lucianocastro <lucianocastro@spacesur.com>
2024-01-16 17:53:24 -0300

    First example commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Montserrat, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const montserrat = Montserrat({ subsets: ['latin'] })
