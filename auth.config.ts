610f9a85025c0fffecfa2b677fa822d3b395cc31
Luciano Castro Ojeda <gluciano@gmail.com>
2024-01-26 13:19:58 -0300

    Fix type problem on auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,18 @@
 import type { NextAuthConfig } from 'next-auth';
+import { NextRequest } from 'next/server';
 
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
+    authorized({
+      auth,
+      request: { nextUrl },
+    }: {
+      auth: { user: string };
+      request: NextRequest;
+    }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
